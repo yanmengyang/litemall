a9910bfc4984c28638ce89905fb0730719047e7e
tukela <767224102@qq.com>
2021-04-19 10:43:47 +0800

    图片上传

---
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxStorageController.java
@@ -0,0 +1,82 @@
+package org.linlinjava.litemall.wx.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.linlinjava.litemall.core.storage.StorageService;
+import org.linlinjava.litemall.core.util.ResponseUtil;
+import org.linlinjava.litemall.core.validator.Order;
+import org.linlinjava.litemall.core.validator.Sort;
+import org.linlinjava.litemall.db.domain.LitemallStorage;
+import org.linlinjava.litemall.db.service.LitemallStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/wx/storage")
+@Validated
+public class WxStorageController {
+    private final Log logger = LogFactory.getLog(WxStorageController.class);
+
+    @Autowired
+    private StorageService storageService;
+    @Autowired
+    private LitemallStorageService litemallStorageService;
+
+
+    @PostMapping("/list")
+    public Object list(String key, String name,
+                       @RequestParam(defaultValue = "1") Integer page,
+                       @RequestParam(defaultValue = "10") Integer limit,
+                       @Sort @RequestParam(defaultValue = "add_time") String sort,
+                       @Order @RequestParam(defaultValue = "desc") String order) {
+        List<LitemallStorage> storageList = litemallStorageService.querySelective(key, name, page, limit, sort, order);
+        return ResponseUtil.okList(storageList);
+    }
+
+
+    @PostMapping("/create")
+    public Object create(@RequestParam("file") MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        LitemallStorage litemallStorage = storageService.store(file.getInputStream(), file.getSize(),
+                file.getContentType(), originalFilename);
+        return ResponseUtil.ok(litemallStorage);
+    }
+
+
+    @PostMapping("/read")
+    public Object read(@NotNull Integer id) {
+        LitemallStorage storageInfo = litemallStorageService.findById(id);
+        if (storageInfo == null) {
+            return ResponseUtil.badArgumentValue();
+        }
+        return ResponseUtil.ok(storageInfo);
+    }
+
+
+    @PostMapping("/update")
+    public Object update(@RequestBody LitemallStorage litemallStorage) {
+        if (litemallStorageService.update(litemallStorage) == 0) {
+            return ResponseUtil.updatedDataFailed();
+        }
+        return ResponseUtil.ok(litemallStorage);
+    }
+
+
+    @PostMapping("/delete")
+    public Object delete(@RequestBody LitemallStorage litemallStorage) {
+        String key = litemallStorage.getKey();
+        if (StringUtils.isEmpty(key)) {
+            return ResponseUtil.badArgument();
+        }
+        litemallStorageService.deleteByKey(key);
+        storageService.delete(key);
+        return ResponseUtil.ok();
+    }
+}

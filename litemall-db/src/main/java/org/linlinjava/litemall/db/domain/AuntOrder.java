be4ae8374203ff6f69eb4fce0de96f45ee950c19
honglei <767224102@qq.com>
2021-05-14 10:44:25 +0800

    时间格式化

--- litemall-db/src/main/java/org/linlinjava/litemall/db/domain/AuntOrder.java
+++ litemall-db/src/main/java/org/linlinjava/litemall/db/domain/AuntOrder.java
@@ -89,6 +89,8 @@ public class AuntOrder implements Serializable {
      */
     private String remark;
 
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date creatTime;
 
     /**
